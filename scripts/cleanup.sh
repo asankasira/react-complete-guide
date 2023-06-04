a1cf7d6fff31c8cc68c97e61c181ef94668ce240
Asanka Siriwardena <asankabg@gmail.com>
2023-06-04 08:36:17 +0530

    Cleanup script for templates

---
+++ scripts/cleanup.sh
@@ -0,0 +1,10 @@
+rm -f  ./package-lock.json
+rm -f  ./pnpm-lock.yaml
+rm -rf ./node_modules
+
+rm -f  ./package-lock.json
+rm -f  ./*.log
+rm -rf ./dist
+rm -rf ./build
+rm -rf ./coverage
+rm -rf ./junit
